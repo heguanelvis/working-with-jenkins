49812b4646b06b6b2c96adc90a449f2be5bbfa17
Guan He <guan.he@slalom.com>
2020-04-06 16:04:36 -0400

    initial setup

---
+++ src/test/java/com/slalom/com/simplewebform/SimpleWebFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slalom.com.simplewebform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
